994e5ac1092ba21015ca788ff458862f946ab20f
Pratik kadam <pratik.kadam4@somayia.edu>
2025-03-09 19:48:21 +0530

    made changes to the link

--- client/api.ts
+++ client/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const API_BASE_URL = process.env.API_URL;
-const url = "https://drone-connect.onrender.com";
+const url = "https://drone-connect-main.onrender.com";
 const API_BASE_URL = `${url}/api/v1`;
 
 
